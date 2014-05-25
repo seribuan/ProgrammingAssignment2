df134611494b5174534c86b62befd19ec0daab50
seribuan <naubires@gmail.com>
2014-05-25 10:55:32 +0700

    Commiting the Cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
+## To use this function, one must call the makeCacheMatrix first
+## then call the cacheSolve
+## The checking wether to use the current data in cache must be done outside this functions
 
-## Write a short comment describing this function
+## this function is used to store the cached values, and the functions to get/set it
 
 makeCacheMatrix <- function(x = matrix()) {
-
+      m <- NULL
+      set <- function(y) {
+            x <<- y
+            m <<- NULL
+      }
+      get <- function() x
+      setinverse <- function(mean) m <<- mean
+      getinverse <- function() m
+      list(set = set, get = get,
+           setinverse = setinverse,
+           getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## this function get the invert of the matrix
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+      ## Return a matrix that is the inverse of 'x'
+      m <- x$getinverse()
+      if(!is.null(m) ) {
+            message("getting cached data")
+            return(m)
+      }
+      data <- x$get()
+      m <- solve(data, ...)
+      x$setinverse(m)
+      m
 }
